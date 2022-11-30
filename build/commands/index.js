7d3b5365890f567a6242f931ce83d271b080d175
MattHickson <matt.hickson.a@gmail.com>
2022-11-30 10:01:35 -0600

    Remove Build folder from GIT

--- build/commands/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.divinesfound = exports.help = void 0;
-exports.help = require("./help");
-exports.divinesfound = require("./divinesfound");
-//# sourceMappingURL=index.js.map
